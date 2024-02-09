e7967a69238ca03c1290110969713c1bee5e7c88
ryandvl <contatoryandvl55@gmail.com>
2024-02-09 15:49:46 -0300

    Segunda aula

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,25 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+from src.controllers.tag_creator_controller import TagCreatorController
+
+class TagCreatorView:
+    '''
+        Responsability for interacting with HTTP
+    '''
+
+    def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+
+        if not body or body["product_code"]:
+            return HttpResponse(status_code=400, body={"message": "Body not found"})
+
+        product_code = body["product_code"]
+
+        # Business rule logic
+        formatted_response = tag_creator_controller.create(product_code)
+
+        # Return HTTP
+        return HttpResponse(status_code=200, body=formatted_response)
